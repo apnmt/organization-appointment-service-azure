881cfdd6ad47bb3a3c4bfce42aa38d92db1ab3ff
TWI <audi-connect@msg.group>
2022-01-11 10:15:28 +0100

    initial commit

---
+++ src/test/java/de/apnmt/organizationappointment/organizationappointmentserviceaws/OrganizationAppointmentServiceAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.apnmt.organizationappointment.organizationappointmentserviceaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationAppointmentServiceAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
