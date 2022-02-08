520d378a57a10128ef032ae621d6192c1ca8b668
tobi5775 <tobias.wittmann96@web.de>
2022-02-08 18:52:10 +0100

    change subscription names

--- src/main/java/de/apnmt/organizationappointment/messaging/SubscriptionConstants.java
+++ src/main/java/de/apnmt/organizationappointment/messaging/SubscriptionConstants.java
@@ -6,9 +6,9 @@ public class SubscriptionConstants {
 
     public static final String SERVICE_CHANGED_SUBSCRIPTION = "orgapnmt-service-changed-subscription";
 
-    public static final String CLOSING_TIME_CHANGED_SUBSCRIPTION = "closing-time-changed-subscription";
+    public static final String CLOSING_TIME_CHANGED_SUBSCRIPTION = "orgapnmt-closing-time-changed-subscription";
 
-    public static final String OPENING_HOUR_CHANGED_SUBSCRIPTION = "opening-hour-changed-subscription";
+    public static final String OPENING_HOUR_CHANGED_SUBSCRIPTION = "orgapnmt-opening-hour-changed-subscription";
 
-    public static final String WORKING_HOUR_CHANGED_SUBSCRIPTION = "working-hour-changed-subscription";
+    public static final String WORKING_HOUR_CHANGED_SUBSCRIPTION = "orgapnmt-working-hour-changed-subscription";
 }
